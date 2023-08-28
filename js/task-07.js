ae89997195c65b5ce5bb3993808f229c122aa7c4
DenysTkachov <D.Tkachev@ukr.net>
2023-08-28 21:26:14 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,10 @@
 const fontSizeControl = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
 
+
+const initialFontSize = fontSizeControl.value + "px";
+text.style.fontSize = initialFontSize;
+
 fontSizeControl.addEventListener("input", () => {
   const fontSize = fontSizeControl.value + "px";
   text.style.fontSize = fontSize;
