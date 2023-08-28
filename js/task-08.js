f335efaa74299e0a83272e2474a1d5d0108d0d3b
DenysTkachov <D.Tkachev@ukr.net>
2023-08-28 15:24:38 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("All fields must be filled");
+    return;
+  }
+
+  const formData = {
+    email: email,
+    password: password,
+  };
+
+  console.log(formData);
+  form.reset();
+}
