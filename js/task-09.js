f10f411960b267cf3a0a0f0e53b8b1793e2a34af
DenysTkachov <D.Tkachev@ukr.net>
2023-08-28 15:58:43 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const button = document.querySelector(".change-color");
+const bodyBg = document.querySelector("body");
+const colorName = document.querySelector(".color")
+
+button.addEventListener(`click`, onClickButton);
+function onClickButton() {
+  const randomColor = getRandomHexColor();
+  bodyBg.style.backgroundColor = randomColor;
+  colorName.textContent = randomColor;
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
